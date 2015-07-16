a03e69b46fd69511d78d2550fffa315fe8bc06d7
alex <alex@nabu.be>
2015-07-16 11:00:50 +0200

    Updated validator to use generic validation

--- src/main/java/be/nabu/libs/validator/api/Validator.java
+++ src/main/java/be/nabu/libs/validator/api/Validator.java
@@ -3,6 +3,6 @@ package be.nabu.libs.validator.api;
 import java.util.List;
 
 public interface Validator<T> {
-	public List<ValidationMessage> validate(T instance);
+	public List<? extends Validation<?>> validate(T instance);
 	public Class<T> getValueClass();
 }
